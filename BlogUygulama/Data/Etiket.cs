8905ffda91e035afb3c3b9b8c93541a6d18baabd
GayeKORDACI <kordacig@gmail.com>
2016-07-26 16:12:44 +0300

    BlogUygulaması

---
+++ BlogUygulama/Data/Etiket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogUygulama.Data
+{
+    public class Etiket
+    {
+        public int EtiketId { get; set; }
+
+        [Required(ErrorMessage = "Lütfen etiketin içeriğini giriniz.")]
+        [StringLength(50, ErrorMessage = "Etiketin içeriği 50 karakterden uzun olamaz.")]
+        public string Icerik { get; set; }
+
+        //Aynı etiket, birden çok makale de kullanılıyor olabilir.
+        public virtual List<Makale> Makales { get; set; }
+    }
+}
---
+++ BlogUygulama/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogUygulama.Startup))]
+namespace BlogUygulama
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+          
+        }
+    }
+}
